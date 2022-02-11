e0e1c4274c243a0eb7d2f7525a29698d7c50233f
Marlon Müller <marmueller@united-internet.de>
2022-02-11 15:35:29 +0100

    Update action

--- .github/actions/deploy-now-configuration-template-action/cmd/root_test.go
+++ .github/actions/deploy-now-configuration-template-action/cmd/root_test.go
@@ -93,6 +93,24 @@ func TestMultipleData(t *testing.T) {
 	test.AssertEqual(t, "A=1\nC=3", string(file))
 }
 
+func TestObject(t *testing.T) {
+	defer os.RemoveAll(TempPath)
+
+	var buffer bytes.Buffer
+	rootCmd := NewRootCmd()
+	rootCmd.SetOut(&buffer)
+	rootCmd.SetArgs([]string{"-i", "../test/data/templates", "-o", testDir(t), "-d", yamlData1, "-t", ".template4"})
+
+	err := rootCmd.Execute()
+
+	test.AssertEqual(t, nil, err)
+	test.AssertEqual(t, "", buffer.String())
+
+	file, err := os.ReadFile(testDir(t) + "/test.txt")
+	test.AssertEqual(t, nil, err)
+	test.AssertEqual(t, "A={\"a\":1,\"b\":{\"c\":2}}\nC={\"c\":2}", string(file))
+}
+
 func TestMissingData(t *testing.T) {
 	defer os.RemoveAll(TempPath)
 
@@ -105,6 +123,24 @@ func TestMissingData(t *testing.T) {
 
 	test.AssertNotEqual(t, nil, err)
 	test.AssertEqual(t, "template: test.txt.template3:2:5: executing \"test.txt.template3\" at <.c>: map has no entry for key \"c\"", err.Error())
+}
+
+func TestRuntimeData(t *testing.T) {
+	defer os.RemoveAll(TempPath)
+
+	var buffer bytes.Buffer
+	rootCmd := NewRootCmd()
+	rootCmd.SetOut(&buffer)
+	rootCmd.SetArgs([]string{"-i", "../test/data/templates", "-o", testDir(t), "--runtime-data", yamlData1})
+
+	err := rootCmd.Execute()
+
+	test.AssertEqual(t, nil, err)
+	test.AssertEqual(t, "Runtime values used while rendering templates:\n1\n2", buffer.String())
+
+	file, err := os.ReadFile(testDir(t) + "/test.txt")
+	test.AssertEqual(t, nil, err)
+	test.AssertEqual(t, "A=1\nB=2", string(file))
 }
 
 func testDir(t *testing.T) string {
